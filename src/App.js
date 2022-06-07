6b64b7d7ea7efd1fcd41f68f478af52dba42dbbe
Andrew Smith <meuzishun@gmail.com>
2022-06-07 12:31:08 -0400

    End of video 9
    Pass props and children into components.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        {/* <Greet /> */}
-        {/* <Welcome /> */}
-        <Hello />
+        <Greet name='Bruce' heroName='Batman'>
+          <p>This is children props</p>
+        </Greet>
+        <Greet name='Clark' heroName='Superman'>
+          <button>Action</button>
+        </Greet>
+        <Greet name='Diana' heroName='Wonder Woman' />
+        <Welcome name='Bruce' heroName='Batman' />
+        <Welcome name='Clark' heroName='Superman' />
+        <Welcome name='Diana' heroName='Wonder Woman' />
+        {/* <Hello /> */}
       </div>
     );
   }
