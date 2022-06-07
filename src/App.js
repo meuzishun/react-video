960dd26b23dacf4871fb77502c5bbe2ece1c7af3
Andrew Smith <meuzishun@gmail.com>
2022-06-07 14:55:56 -0400

    End of video 12
    Use destructuring to pull out prop and state names to clarify code.

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Counter />
+        {/* <Counter /> */}
         {/* <Message /> */}
         {/* <Greet name='Bruce' heroName='Batman'>
           <p>This is children props</p>
         </Greet>
         <Greet name='Clark' heroName='Superman'>
           <button>Action</button>
-        </Greet>
+        </Greet> */}
         <Greet name='Diana' heroName='Wonder Woman' />
         <Welcome name='Bruce' heroName='Batman' />
-        <Welcome name='Clark' heroName='Superman' />
+        {/* <Welcome name='Clark' heroName='Superman' />
         <Welcome name='Diana' heroName='Wonder Woman' /> */}
         {/* <Hello /> */}
       </div>
