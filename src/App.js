4d84198d846542e23f16d09216e6978f0a97859d
Andrew Smith <meuzishun@gmail.com>
2022-06-10 20:40:29 -0400

    End of video 30
    Use forwardRef to pass a reference down to child from parent.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 import RefsDemo from './components/RefsDemo';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <FRParentInput />
+        {/* <FocusInput /> */}
         {/* <RefsDemo /> */}
-        <FocusInput />
       </div>
     );
   }
