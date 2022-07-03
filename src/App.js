c8262be3b57dae372687bd6902d3fb9693dd5504
Andrew Smith <meuzishun@gmail.com>
2022-07-03 15:54:05 -0400

    Demonstrate ErrorBoundary and Higher-Order Components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
-import RefsDemo from './components/RefsDemo';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <FRParentInput />
-        {/* <FocusInput /> */}
-        {/* <RefsDemo /> */}
+        <ClickCounter />
+        <HoverCounter />
       </div>
     );
   }
